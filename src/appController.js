3f91a73ffce6ee0e0a6bcc558b522bae30d75c67
Dren Morina <drenmorina321@gmail.com>
2024-11-30 19:57:49 +0100

    Create functions to create and render the todos

--- src/appController.js
+++ src/appController.js
@@ -1,18 +1,26 @@
 import { Project, Todo } from "./dataModel";
+import { renderTodos } from "./domManager";
 
 let project1 = new Project("Project 1");
 
 let todo1 = new Todo("Todo1", "today", 1);
 let todo2 = new Todo("Todo2", "tomorrow", 2);
+let todo3 = new Todo("SEWYYY", "1", 1);
 
 project1.addTodo(todo1);
 project1.addTodo(todo2);
+project1.addTodo(todo3);
 
 todo1.toggleCompleted();
 todo1.addDescription("Helloooo");
 
 function test() {
-  console.log(project1.getTodos());
+  let todoList = project1.getTodos();
+  todoList.forEach((todo) => {
+    // console.log(todo.title);
+    // console.log("__________");
+  });
+  renderTodos(todoList);
 }
 
 export { test };
--- src/domManager.js
+++ src/domManager.js
@@ -6,4 +6,39 @@ function closeDialog(dialog) {
   dialog.close();
 }
 
-export { showDialog, closeDialog };
+function createTodoElement(todo) {
+  const todoElement = document.createElement("li");
+  todoElement.classList.add("todo-item");
+
+  const statusCircle = document.createElement("div");
+  statusCircle.classList.add("status-circle");
+
+  const todoTitle = document.createElement("p");
+  todoTitle.classList.add("todo-title");
+  todoTitle.textContent = todo.title;
+
+  todoElement.appendChild(statusCircle);
+  todoElement.appendChild(todoTitle);
+
+  return todoElement;
+}
+
+function createTodoList() {
+  const todoList = document.createElement("ul");
+  todoList.setAttribute("id", "todo-list");
+
+  return todoList;
+}
+
+function renderTodos(todoArray) {
+  const todoContainer = document.querySelector("#todo-container");
+  const todoList = createTodoList();
+  todoArray.forEach((todo) => {
+    const todoElement = createTodoElement(todo);
+    todoList.appendChild(todoElement);
+  });
+
+  todoContainer.insertBefore(todoList, todoContainer.firstChild);
+}
+
+export { showDialog, closeDialog, renderTodos };
