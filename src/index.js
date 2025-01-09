73d4c3892256a27849f8dd200c083bec367c50f9
Dren Morina <drenmorina321@gmail.com>
2025-01-09 16:25:36 +0100

    Add todo list. Finish prototype version

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import { initEvents } from "./eventManager";
 
 initializeApp();
 initEvents();
+
+/* TODO's
+Make date/time choseable when creating todo
+Format date and time correctly
+Make priority choseable when creating todo
+Show date/time and priority in todo details
+Make default project not deleteable
+Make todos swapable
+*/
